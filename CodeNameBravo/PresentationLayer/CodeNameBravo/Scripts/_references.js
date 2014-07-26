c78095d933646bff7ac51063d4879f9ac95bf679
aholm <holmslice86@gmail.com>
2014-07-26 12:28:20 -0700

    Fixed references issues

--- CodeNameBravo/PresentationLayer/CodeNameBravo/Scripts/_references.js
+++ CodeNameBravo/PresentationLayer/CodeNameBravo/Scripts/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="jquery-2.1.1.js" />
+/// <reference path="jquery.validate.js" />
 /// <reference path="app/app-config.js" />
 /// <reference path="app/app.js" />
 /// <reference path="app/_dependencies/angular-route.js" />
